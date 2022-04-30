752a2089f4ac488dde646fca463df8cafadef9c3
Evren Ceyhan <evren@ceyhan.io>
2022-04-30 16:14:00 +0300

    app contents moved to Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,6 @@
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { SearchAppBar } from './components/AppBar';
-import { TourCard } from './components/TourCard';
-import cities from './data.json';
+import { Home } from './pages/Home';
 import './App.css';
 
 export const App = () => {
-    return (
-        <div className="App">
-            <SearchAppBar />
-            <Container sx={{ marginY: 5 }}>
-                {cities.map((city) => (
-                    <>
-                        <Typography variant="h4" mt={5} mb={3}>
-                            Top {city.name} Tours
-                        </Typography>
-                        <Grid container spacing={5}>
-                            {city.tours.map((tour, index) => (
-                                <TourCard tour={tour} key={index} />
-                            ))}
-                        </Grid>
-                    </>
-                ))}
-            </Container>
-        </div>
-    );
+    return <Home />;
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { SearchAppBar } from '../components/AppBar';
+import { TourCard } from '../components/TourCard';
+import cities from '../data.json';
+
+export const Home = () => {
+    return (
+        <div>
+            <SearchAppBar />
+            <Container sx={{ marginY: 5 }}>
+                {cities.map((city) => (
+                    <>
+                        <Typography variant="h4" mt={5} mb={3}>
+                            Top {city.name} Tours
+                        </Typography>
+                        <Grid container spacing={5}>
+                            {city.tours.map((tour, index) => (
+                                <TourCard tour={tour} key={index} />
+                            ))}
+                        </Grid>
+                    </>
+                ))}
+            </Container>
+        </div>
+    );
+};
