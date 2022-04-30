a8641126741bb1fc759bfd6e53fc6ac593fa5052
Evren Ceyhan <evren@ceyhan.io>
2022-04-30 15:57:07 +0300

    appbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import { SearchAppBar } from './components/AppBar';
 import { TourCard } from './components/TourCard';
 import './App.css';
 
 export const App = () => {
     return (
         <div className="App">
-            <Container>
+            <SearchAppBar />
+            <Container sx={{ marginY: 5 }}>
                 <Grid container spacing={5}>
                     <TourCard />
                     <TourCard />
