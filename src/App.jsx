66cbc5007fedd15cd5a514c8ac7b700938a9b3c3
Evren Ceyhan <evren@ceyhan.io>
2022-04-30 16:20:34 +0300

    basic routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { Home } from './pages/Home';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
-    return <Home />;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
