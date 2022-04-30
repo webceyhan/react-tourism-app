5459ac69e18aaafff4984c074e6e3f2f5a931288
Evren Ceyhan <evren@ceyhan.io>
2022-04-30 17:03:15 +0300

    basic tour page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { SearchAppBar } from './components/AppBar';
 import { Home } from './pages/Home';
+import { Tour } from './pages/Tour';
 import './App.css';
 
 export const App = () => {
@@ -9,6 +10,7 @@ export const App = () => {
             <SearchAppBar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/tour/:id" element={<Tour />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/pages/Tour.jsx
@@ -0,0 +1,12 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+export const Tour = () => {
+    return (
+        <Container sx={{ marginY: 5 }}>
+            <Typography variant="h3" mt={5} mb={3}>
+                Explore the world
+            </Typography>
+        </Container>
+    );
+};
