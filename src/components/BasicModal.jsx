187e7a48bbc2ea7ba1df85433e61f3d88520d3d3
Evren Ceyhan <evren@ceyhan.io>
2022-04-30 18:38:38 +0300

    datapickers wrapped with labels and borders

--- src/components/BasicModal.jsx
+++ src/components/BasicModal.jsx
@@ -2,14 +2,15 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
 import { DatePicker } from './DatePicker';
 
 const style = {
+    display: 'flex',
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
@@ -36,7 +37,19 @@ export const BasicModal = () => {
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
-                    <DatePicker />
+                    <Box mb={2} borderRight={1} mr={2}>
+                        <Typography variant="h6" mb={2} color="warning.main">
+                            From
+                        </Typography>
+                        <DatePicker />
+                    </Box>
+
+                    <Box mb={2}>
+                        <Typography variant="h6" mb={2} color="warning.main">
+                            To
+                        </Typography>
+                        <DatePicker />
+                    </Box>
                 </Box>
             </Modal>
         </Box>
