f66e2f6cd92fe8390c47401bd8b9773f0e788d60
Evren Ceyhan <evren@ceyhan.io>
2022-04-30 17:13:38 +0300

    tour picture and collage added

--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -1,12 +1,26 @@
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { ImageCollage } from '../components/ImageCollage';
 
 export const Tour = () => {
     return (
         <Container sx={{ marginY: 5 }}>
-            <Typography variant="h3" mt={5} mb={3}>
-                Explore the world
+            <Typography variant="h4" mt={5} mb={3}>
+                Explore the world in Vegas
             </Typography>
+            <Box
+                mt={3}
+                sx={{
+                    display: 'flex',
+                }}
+            >
+                <img
+                    src="https://media.timeout.com/images/105124791/750/422/image.jpg"
+                    height={325}
+                />
+                <ImageCollage />
+            </Box>
         </Container>
     );
 };
